70f585a005c8158467bb78e42d04c023bacf0f22
Saad-iit <77770818+Saad-iit@users.noreply.github.com>
2021-04-13 07:05:16 -0500

    added js script to show alert

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-
- 
-
-
-
-
-
-
-
+	var entry = document.getElementById("entrybutton");
+	var txtInput = document.getElementById("entryinput");
+	var txtOutput = document.getElementById("textoutput");
+	
+	function start(){
+		alert("Saad: " + txtInput.value);
+		txtOutput.innerHTML = txtInput.value;
+	}
+   entry.addEventListener('click',start);
+}
 window.addEventListener('load', init);
